b45c6a856b09957e96e26d5296f29038649d2822
kolbeckj0387 <kolbeckj0387@my.uwstout.edu>
2017-04-20 16:50:43 -0500

    Old code gone and changed intent
    Got rid of the old "enter text" and send button that was useless to our project. I also changed the "plan workout" button to now go to the user profile page so the app is much smoother.

--- app/src/main/java/uwstout/healmebaby/MainActivity.java
+++ app/src/main/java/uwstout/healmebaby/MainActivity.java
@@ -19,19 +19,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    /** Called when a user clicks the Send button */
-    public void sendMessage(View view) {
-        //Do something in response to button
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
     /** go to the planner page */
-    public void plannerPage(View view) {
-        Intent planner_Intent = new Intent(this, PlannerActivity.class);
+    public void userPage(View view) {
+        Intent planner_Intent = new Intent(this, UserProfile.class);
         startActivity(planner_Intent);
     }
 
