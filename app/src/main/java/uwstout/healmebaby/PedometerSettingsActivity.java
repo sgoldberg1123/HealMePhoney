592b53a3ba7fa6b977813fed4c0cd0271557fe02
Student <Student>
2017-04-24 16:33:18 -0500

    Pedometer updated

---
+++ app/src/main/java/uwstout/healmebaby/PedometerSettingsActivity.java
@@ -0,0 +1,10 @@
+package uwstout.healmebaby;
+
+import Activity;
+
+/**
+ * Created by Student on 4/23/2017.
+ */
+
+public class PedometerSettingsActivity implements Activity {
+}
---
+++ app/src/main/java/uwstout/healmebaby/PedometerStepDetector.java
@@ -0,0 +1,8 @@
+package uwstout.healmebaby;
+
+/**
+ * Created by Student on 4/23/2017.
+ */
+
+public class PedometerStepDetector {
+}
